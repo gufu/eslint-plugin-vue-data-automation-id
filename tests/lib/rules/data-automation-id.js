68c309d73adef9651b57b6cb2c9ee646b37290cb
Konrad Gusz <k.gusz@cinema-city.pl>
2019-06-25 11:00:47 +0200

    initial commit

---
+++ tests/lib/rules/data-automation-id.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Clickable elements should have a data-automation-id attribute
+ * @author Konrad Gusz
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/data-automation-id'),
+
+  RuleTester = require('eslint').RuleTester
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester()
+ruleTester.run('data-automation-id', rule, {
+
+  valid: [
+    // give me some code that won't trigger a warning
+    {
+      code: '<a href="" data-automation-id="link-name"></a>'
+    }
+  ],
+
+  invalid: [
+    {
+      code: '<a href=""></a>',
+      errors: [{
+        message: 'Fill me in.',
+        type: 'Me too'
+      }]
+    }
+  ]
+})
